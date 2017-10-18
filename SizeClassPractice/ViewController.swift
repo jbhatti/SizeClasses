e268d177e9dd86042a5d1ef6ab311e3a34ac00b0
Jaison Bhatti <jaison.bhatti@gmail.com>
2017-10-18 18:35:33 -0400

    first commit

---
+++ SizeClassPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SizeClassPractice
+//
+//  Created by Jaison Bhatti on 2017-10-18.
+//  Copyright © 2017 Jaison Bhatti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
